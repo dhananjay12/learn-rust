50d69ab02d05f7c7ba5fe241c8f5354af504d869
Dhananjay Singh <dhananjay@backbase.com>
2022-10-02 23:55:39 +0200

    rust

--- concepts/ownership/src/main.rs
+++ concepts/ownership/src/main.rs
@@ -3,9 +3,12 @@ fn main() {
 
     let s1 = String::from("abc");
    
+    let x1 = 5;
+    let y1 = x1; // Its a Copy and not move for primitive types that live on stack. (Because of a trait)
+    println!("x is {} and y is {}", x1, y1);
 
     // let s2 = s1;
-    // print!("{}", s1); // Error - borrow of moved value s1
+    // print!("{}", s1); // Error - borrow of moved value s1. Because istead of a shallow copy like in Java, it moves the value to the new reference.
 
     let s2 = s1.clone();
     println!("{}", s1); // Works fine
@@ -15,8 +18,12 @@ fn main() {
     // do_stuff1(s3);
     // print!("{}", s3); // Error - moved
 
-    do_stuff2(&s3);
-    println!("{}", s3); // Works fine
+    let x2 = 5;
+    do_stuff1_primitive(x2);
+    println!("x after do_stuff1_primitive {}", x2); // Allowed because stack variable are copied not moved like String which is on heap
+
+    let len = do_stuff2(&s3);
+    println!("legth for s3 {} is {}", s3, len); // Works fine
 
     let mut s4 = String::from("xyz");
 
@@ -27,12 +34,17 @@ fn main() {
     println!("{}", s4); // Works fine
 }
 
-// fn do_stuff1(s : String){
-//     s;
-// }
+fn do_stuff1(s : String){
+    s;
+}
 
-fn do_stuff2(s : &String){
+fn do_stuff1_primitive(some_integer : i32){
+    println!("some_integer {}", some_integer);
+}
+
+fn do_stuff2(s : &String) -> usize{
     println!("{}", s);
+    s.len()
 }
 
 // fn do_stuff3(s : &String){
--- concepts/referencing/src/main.rs
+++ concepts/referencing/src/main.rs
@@ -1,3 +1,56 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("abc1");
+    do_stuff1(&s1);
+    println!("{}", s1);
+
+    reference_example1();
+
+    // Example 2
+    // let mut s2 = String::from("abc2");
+    // do_stuff2(&s2);
+    // println!("{}", s2);
+
+    // Example 3
+    let mut s3 = String::from("abc3");
+    do_stuff3(&mut s3);
+    println!("{}", s3);
+
+    // Example 4
+    let mut s4 = String::from("abc4");
+    do_stuff4(&mut s4);
+    println!("{}", s4);
+}
+
+fn do_stuff1(s: &String){ // Reference , gets moved to the funtion
+    println!("do_stuff {}", s); 
+}
+
+// // Example 2
+// fn do_stuff2(s: &String){
+//     s.insert_str(0, "Hi"); // Error `s` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+//     println!("do_stuff2 {}", s); 
+// }
+
+// Example 3
+fn do_stuff3(s: &mut String){
+    s.insert_str(0, "Hi "); // Change the original string too
+    println!("do_stuff3 {}", s); 
+}
+
+// Example 4
+fn do_stuff4(s: &mut String){
+    (*s).insert_str(0, "Hi "); // Change the original string too
+    println!("do_stuff4 {}", s); 
+}
+
+fn reference_example1(){
+    let mut x = 10;
+
+    let dom = &mut x; //Mutable reference to x, we can change x through dom
+
+    //dom += 1; // Error `+=` cannot be applied to type `&mut {integer}`
+
+    *dom += 1;
+
+    println!("x is {}", x)
 }
